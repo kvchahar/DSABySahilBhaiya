efe09eaa9956cc0895d415adfef201db886cec4c
krishnaveer <krishnaveer.chahar_cs19@gla.ac.in>
2021-11-17 00:18:07 +0530

    Min Cost Climbing Stairs

---
+++ src/dynamicprogramming/MinCostClimbingStairs.java
@@ -0,0 +1,35 @@
+package dynamicprogramming;
+
+import java.util.HashMap;
+
+public class MinCostClimbingStairs {
+
+    public static int minCostClimbingStairs(int[] cost) {
+        int a = minCost(cost, 0, new HashMap<>());
+        int b = minCost(cost, 1,new HashMap<>());
+        return Math.min(a, b);
+    }
+    public static int minCost(int[] cost, int currentIndex, HashMap<Integer,Integer> memo) {
+        if (currentIndex == cost.length) {
+            return 0;
+        }
+        if (currentIndex > cost.length) {
+            return 1000;
+        }
+
+        int currentKey = currentIndex;
+        if(memo.containsKey(currentKey)){
+            return memo.get(currentKey);
+        }
+
+        int oneJump = cost[currentIndex] + minCost(cost, currentIndex + 1,memo);
+        int twoJumps = cost[currentIndex] + minCost(cost, currentIndex + 2,memo);
+
+        memo.put(currentIndex,Math.min(oneJump,twoJumps));
+        return Math.min(oneJump, twoJumps);
+    }
+    public static void main(String[] args) {
+        int[] cost = {10, 15, 20};
+        System.out.println(minCostClimbingStairs(cost));
+    }
+}
