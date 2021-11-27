e679d7bbd53ccc7e67c7aaff873f554170b9563e
krishnaveer <krishnaveer.chahar_cs19@gla.ac.in>
2021-11-27 19:01:04 +0530

    Jump Game II

---
+++ src/dynamicprogramming/JumpGameII.java
@@ -0,0 +1,41 @@
+package dynamicprogramming;
+
+import java.util.HashMap;
+
+public class JumpGameII {
+    public static int jump(int[] nums) {
+        return isPossible(nums,0, new HashMap<>());
+    }
+
+    public static int isPossible(int[] nums, int currentIndex, HashMap<Integer,Integer> memo){
+
+
+        if(currentIndex>=nums.length-1){
+            return 0;
+        }
+
+        int currentKey = currentIndex;
+
+        if(memo.containsKey(currentKey)){
+            return memo.get(currentKey);
+        }
+
+
+        int currentValue = nums[currentIndex];
+        int ans = 10001;
+
+        for(int i=1; i<=currentValue; i++){
+
+            int temp = 1 + isPossible(nums, currentIndex + i, memo);
+            ans = Math.min(ans,temp);
+            memo.put(currentKey,ans);
+
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums =  {2,3,1,1,4};
+        System.out.println(jump(nums)); // 2
+    }
+}
