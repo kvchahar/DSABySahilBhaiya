ed76bc8d0fe26b66122052b37121d21a42b04359
krishnaveer <krishnaveer.chahar_cs19@gla.ac.in>
2021-12-02 14:33:06 +0530

    Best Time to Buy and Sell Stock

--- src/dynamicprogramming/BestTimeToBuyAndSellStock.java
+++ src/dynamicprogramming/BestTimeToBuyAndSellStock.java
@@ -32,6 +32,6 @@ public class BestTimeToBuyAndSellStock {
 
     public static void main(String[] args) {
         int[] nums = {7,1,5,3,6,4};
-        System.out.println(maxProfit(nums)); // 5
+        System.out.println(maxProfit(nums)); //5
     }
 }
