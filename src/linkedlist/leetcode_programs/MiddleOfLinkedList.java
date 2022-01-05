ed46ebbb44d8fec54e45088d7e477e7e65c2d3cc
user <krishnaveer.chahar_cs19@gla.ac.in>
2022-01-05 19:03:55 +0530

    Middle of Linked List

---
+++ src/linkedlist/leetcode_programs/MiddleOfLinkedList.java
@@ -0,0 +1,18 @@
+package linkedlist.leetcode_programs;
+
+import linkedlist.leetcode_programs.node.ListNode;
+
+public class MiddleOfLinkedList {
+    public ListNode middleNode(ListNode head) {
+        ListNode fastPtr = head;
+        ListNode slowPtr = head;
+        while(fastPtr.next!=null && fastPtr.next.next!=null){
+            fastPtr = fastPtr.next.next;
+            slowPtr = slowPtr.next;
+        }
+        if(fastPtr.next!=null){
+            return slowPtr.next;
+        }
+        return slowPtr;
+    }
+}
