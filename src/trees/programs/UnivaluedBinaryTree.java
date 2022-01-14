758b8a07fb464c8287124d7e095657589edfb385
user <krishnaveer.chahar_cs19@gla.ac.in>
2022-01-14 18:17:26 +0530

    Sum of Root to Leaf Binary Numbers

---
+++ src/trees/programs/UnivaluedBinaryTree.java
@@ -0,0 +1,26 @@
+package trees.programs;
+
+import trees.TreeNode.TreeNode;
+
+public class UnivaluedBinaryTree {
+    public boolean isUnivalTree(TreeNode root) {
+
+        return isSame(root, root.val);
+    }
+
+    public boolean isSame(TreeNode root, int value) {
+
+        if (root == null) {
+            return true;
+        }
+
+        if (root.val != value) {
+            return false;
+        }
+
+        boolean left = isSame(root.left, value);
+        boolean right = isSame(root.right, value);
+
+        return left && right;
+    }
+}
