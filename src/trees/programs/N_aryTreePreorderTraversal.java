6f6cd6b8db27a900901643c2f6aedac491970e86
user <krishnaveer.chahar_cs19@gla.ac.in>
2022-01-16 16:22:40 +0530

    N-ary Tree Preorder Traversal by Recursive and Iterative

---
+++ src/trees/programs/N_aryTreePreorderTraversal.java
@@ -0,0 +1,62 @@
+package trees.programs;
+
+import trees.TreeNode.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class N_aryTreePreorderTraversal {
+//     public List<Integer> preorder(Node root) {
+//         List<Integer> list = new ArrayList<>();
+//         postOrderTraversal(root,list);
+
+//         return list;
+//      }   
+
+//     public void preOrderTraversal(Node root, List<Integer> list){
+
+//         if(root==null){
+//             return;
+//         }
+
+
+//        for(Node child : root.children){
+//            preOrderTraversal(child,list);
+//        }
+//          list.add(root.val);
+
+//         return;
+//     }
+
+    public List<Integer> preorder(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        preOrderTraversal(root, list);
+
+        return list;
+    }
+
+    public void preOrderTraversal(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode current = stack.pop();
+            list.add(current.val);
+
+            List<TreeNode> currentNode = current.children;
+
+            int size = currentNode.size();
+
+            for (int i = size - 1; i >= 0; i--) {
+                stack.push(currentNode.get(i));
+            }
+        }
+
+    }
+}
