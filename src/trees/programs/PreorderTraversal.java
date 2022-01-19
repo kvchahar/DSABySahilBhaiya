a8b9d96007a466cd9471da0a61ee4b9eecceb464
user <krishnaveer.chahar_cs19@gla.ac.in>
2022-01-19 16:06:32 +0530

    Preorder Traversal

---
+++ src/trees/programs/PreorderTraversal.java
@@ -0,0 +1,32 @@
+package trees.programs;
+
+import trees.TreeNode.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class PreorderTraversal {
+    // Return a list containing the inorder traversal of the given tree
+    ArrayList<Integer> preOrder(TreeNode root) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        Stack<TreeNode> stack = new Stack<>();
+
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode current = stack.pop();
+            list.add(current.val);
+
+            if (current.right != null) {
+                stack.push(current.right);
+            }
+
+            if (current.left != null) {
+                stack.push(current.left);
+            }
+        }
+
+        return list;
+    }
+}
