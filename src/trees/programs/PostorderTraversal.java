604401b2b597a1c82b113360a4078a9d8e4c0b33
user <krishnaveer.chahar_cs19@gla.ac.in>
2022-01-19 16:08:46 +0530

    Postorder Traversal

---
+++ src/trees/programs/PostorderTraversal.java
@@ -0,0 +1,34 @@
+package trees.programs;
+
+import trees.TreeNode.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Stack;
+
+public class PostorderTraversal {
+    ArrayList<Integer> postOrder(TreeNode node) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(node);
+
+        while(!stack.isEmpty()){
+            TreeNode current = stack.pop();
+
+            list.add(current.val);
+
+            if(current.left!=null){
+                stack.push(current.left);
+            }
+
+            if(current.right!=null){
+                stack.push(current.right);
+            }
+        }
+
+        Collections.reverse(list);
+
+        return list;
+    }
+}
