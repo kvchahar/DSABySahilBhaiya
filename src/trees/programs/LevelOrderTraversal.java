615d316d611f48883237a9f0a1c76ab59fb6c952
user <krishnaveer.chahar_cs19@gla.ac.in>
2022-01-19 15:59:42 +0530

    Level Order Traversal

---
+++ src/trees/programs/LevelOrderTraversal.java
@@ -0,0 +1,37 @@
+package trees.programs;
+
+import trees.TreeNode.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LevelOrderTraversal {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> answers = new ArrayList<>();
+
+
+        if(root==null){
+            return answers;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        queue.offer(root);
+
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for(int i = 0; i<size; i++){
+                if(queue.peek().left!=null)
+                    queue.offer(queue.peek().left);
+                if(queue.peek().right!=null)
+                    queue.offer(queue.peek().right);
+                list.add(queue.poll().val);
+            }
+            answers.add(list);
+        }
+        return answers;
+    }
+}
