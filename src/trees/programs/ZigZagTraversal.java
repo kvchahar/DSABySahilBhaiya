8a676d14574cfbadc2e0fca3500bdac645699f7d
user <krishnaveer.chahar_cs19@gla.ac.in>
2022-01-20 16:16:04 +0530

    Zig Zag Traversal

---
+++ src/trees/programs/ZigZagTraversal.java
@@ -0,0 +1,48 @@
+package trees.programs;
+
+import trees.TreeNode.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class ZigZagTraversal {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+
+        List<List<Integer>> answers = new ArrayList<>();
+
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        if(root==null){
+            return answers;
+        }
+
+        queue.offer(root);
+
+        boolean flag = true;
+
+        while(!queue.isEmpty()){
+            List<Integer> list = new ArrayList<>();
+            int size = queue.size();
+
+            for(int i = 0; i<size; i++){
+
+
+                if(queue.peek().left!=null){
+                    queue.offer(queue.peek().left);
+                }
+
+                if(queue.peek().right!=null){
+                    queue.offer(queue.peek().right);
+                }
+
+                if(flag) list.add(queue.poll().val);
+                else list.add(0,queue.poll().val);
+            }
+            flag = !flag;
+            answers.add(list);
+        }
+        return answers;
+    }
+}
