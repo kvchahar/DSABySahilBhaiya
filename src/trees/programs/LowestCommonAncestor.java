07f4ae319d416599550fed2bdc802082cc278fb1
user <krishnaveer.chahar_cs19@gla.ac.in>
2022-01-21 15:24:40 +0530

    Lowest Common Ancestor

---
+++ src/trees/programs/LowestCommonAncestor.java
@@ -0,0 +1,27 @@
+package trees.programs;
+
+import trees.TreeNode.TreeNode;
+
+public class LowestCommonAncestor {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null || root==p || root==q){
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if(left==null){
+            return right;
+        }
+
+        else if(right==null){
+            return left;
+        }
+        else{
+            return root;
+        }
+    }
+
+
+}
