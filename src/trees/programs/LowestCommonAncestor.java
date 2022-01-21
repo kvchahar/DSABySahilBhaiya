bfb3d9860ff26cd532eff5306ab2d86a3db35d1c
user <krishnaveer.chahar_cs19@gla.ac.in>
2022-01-22 02:10:53 +0530

    Lowest Common Ancestor

--- src/trees/programs/LowestCommonAncestor.java
+++ src/trees/programs/LowestCommonAncestor.java
@@ -1,8 +1,41 @@
 package trees.programs;
 
+import trees.TreeNode.Impl;
 import trees.TreeNode.TreeNode;
 
 public class LowestCommonAncestor {
+    public TreeNode root;
+    public void createTree() {
+        TreeNode first = new TreeNode(1);
+        TreeNode second = new TreeNode(2);
+        TreeNode third = new TreeNode(3);
+//        TreeNode fourth = new TreeNode(11);
+//        TreeNode fifth = new TreeNode(null);
+        TreeNode sixth = new TreeNode(4);
+        TreeNode seventh = new TreeNode(5);
+        TreeNode eighth = new TreeNode(8);
+//        TreeNode ninth = new TreeNode(2);
+        TreeNode tenth = new TreeNode(6);
+        TreeNode eleventh = new TreeNode(7);
+//        TreeNode twelfth = new TreeNode(5);
+//        TreeNode thirteen = new TreeNode(1);
+
+
+        root = first;
+        first.left = second;
+        first.right = third;
+//        second.left = ;
+//        second.right = fifth;
+        third.left = sixth;
+        third.right = seventh;
+//        fourth.left = eighth;
+//        fourth.right = ninth;
+//        fifth.left = tenth;
+//        fifth.right = eleventh;
+        sixth.left = eighth;
+        seventh.left = tenth;
+        seventh.right = eleventh;
+    }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if(root==null || root==p || root==q){
             return root;
@@ -10,6 +43,8 @@ public class LowestCommonAncestor {
 
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        System.out.println(root.val);
 
         if(left==null){
             return right;
@@ -21,8 +56,14 @@ public class LowestCommonAncestor {
         else{
             return root;
         }
+
     }
 
-
+    public static void main(String[] args) {
+        LowestCommonAncestor l = new LowestCommonAncestor();
+        l.createTree();
+        l.lowestCommonAncestor(l.root,new TreeNode(7),new TreeNode(8));
+//        System.out.println(l.root.val);
+    }
 
 }
