42efb60abcd082b9761401a2ea3b5d66fc1dbebe
user <krishnaveer.chahar_cs19@gla.ac.in>
2022-01-23 03:34:22 +0530

    BinaryTree Level Order Traversal 2

---
+++ src/trees/programs/BinaryTreeLevelOrderTraversal2.java
@@ -0,0 +1,51 @@
+package trees.programs;
+
+import trees.TreeNode.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeLevelOrderTraversal2 {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+
+        List<List<Integer>> answers = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+
+        reverseLevelOrder(root,answers);
+        // System.out.println(answers);
+        for(int i = 0; i<answers.size(); i++){
+            res.add(0,answers.get(i));
+        }
+        return res;
+    }
+
+    private void reverseLevelOrder(TreeNode root, List<List<Integer>> answers){
+
+        if(root==null){
+            return;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> answer = new ArrayList<>();
+            for(int i = 0; i<size; i++){
+                if(queue.peek().left!=null){
+                    queue.add(queue.peek().left);
+                }
+                if(queue.peek().right!=null){
+                    queue.add(queue.peek().right);
+                }
+                answer.add(queue.poll().val);
+            }
+
+            answers.add(answer);
+
+        }
+
+    }
+}
