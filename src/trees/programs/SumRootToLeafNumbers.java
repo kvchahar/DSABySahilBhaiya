1aba185be7b8b53c594b9f1f8d6fb73002437fcf
user <krishnaveer.chahar_cs19@gla.ac.in>
2022-01-23 02:59:27 +0530

    Sum Root to Leaf Number

---
+++ src/trees/programs/SumRootToLeafNumbers.java
@@ -0,0 +1,37 @@
+package trees.programs;
+
+import trees.TreeNode.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SumRootToLeafNumbers {
+    public int sumNumbers(TreeNode root) {
+
+        int sum = 0;
+        List<String> answers = new ArrayList<>();
+        sumOfNodes(root,"",answers);
+
+        for(int i = 0; i<answers.size(); i++){
+            sum += Integer.parseInt(answers.get(i));
+        }
+        return sum;
+    }
+
+    private void sumOfNodes(TreeNode root, String path, List<String> answers){
+
+        if(root==null){
+            return;
+        }
+
+        if(root.left==null && root.right==null){
+            path += String.valueOf(root.val);
+            answers.add(path);
+            return;
+        }
+
+        sumOfNodes(root.left, path + String.valueOf(root.val), answers);
+        sumOfNodes(root.right, path + String.valueOf(root.val), answers);
+        return;
+    }
+}
