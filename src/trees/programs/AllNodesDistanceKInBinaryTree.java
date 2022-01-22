62c176611aab08d153044ff4b35adc3dc03908d9
user <krishnaveer.chahar_cs19@gla.ac.in>
2022-01-23 00:30:55 +0530

    All Nodes Distance K In Binary Tree

---
+++ src/trees/programs/AllNodesDistanceKInBinaryTree.java
@@ -0,0 +1,51 @@
+package trees.programs;
+
+import trees.TreeNode.TreeNode;
+
+import java.util.*;
+
+public class AllNodesDistanceKInBinaryTree {
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        List<Integer> answer = new ArrayList<>();
+        HashMap<TreeNode,TreeNode> parentMap = new HashMap<>();
+
+        populateParentMap(root,null,parentMap);
+        printKDistanceNodes(target,k,new HashSet<>(),parentMap,answer);
+
+        return answer;
+    }
+
+    private void printKDistanceNodes(TreeNode currentNode, int k, Set<TreeNode> set, HashMap<TreeNode,TreeNode> parentMap, List<Integer> answer){
+
+        if(currentNode==null || set.contains(currentNode) || k<0){
+            return;
+        }
+
+        if(k==0){
+            answer.add(currentNode.val);
+            return;
+        }
+
+        set.add(currentNode);
+
+        printKDistanceNodes(currentNode.left, k-1, set, parentMap, answer);
+        printKDistanceNodes(currentNode.right, k-1, set, parentMap, answer);
+        printKDistanceNodes(parentMap.get(currentNode), k-1, set, parentMap, answer);
+        return;
+
+    }
+
+    private void populateParentMap(TreeNode currentNode, TreeNode currentParent, HashMap<TreeNode,TreeNode> parentMap){
+
+        if(currentNode==null){
+            return;
+        }
+
+        parentMap.put(currentNode,currentParent);
+
+        populateParentMap(currentNode.left, currentNode, parentMap);
+        populateParentMap(currentNode.right, currentNode, parentMap);
+
+        return;
+    }
+}
