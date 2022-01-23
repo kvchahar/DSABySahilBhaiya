69a86ee9419a9876552581a3c2fed07add36586c
user <krishnaveer.chahar_cs19@gla.ac.in>
2022-01-23 18:12:12 +0530

    Subtree of another Tree

--- src/trees/programs/SubTreeOfAnotherTree.java
+++ src/trees/programs/SubTreeOfAnotherTree.java
@@ -4,21 +4,21 @@ import trees.TreeNode.TreeNode;
 
 public class SubTreeOfAnotherTree {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if(root==null){
+        if (root == null) {
             return false;
-        }else if(isSameSubRoot(root,subRoot)){
+        } else if (isSameSubRoot(root, subRoot)) {
             return true;
-        }else{
-            return isSubtree(root.left,subRoot) || isSubtree(root.right, subRoot);
+        } else {
+            return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
         }
     }
 
-    public boolean isSameSubRoot(TreeNode root, TreeNode subRoot){
-        if(root==null || subRoot==null){
-            return root==null && subRoot==null;
-        }else if(root.val==subRoot.val){
-            return isSameSubRoot(root.left,subRoot.left) && isSameSubRoot(root.right, subRoot.right);
-        }else{
+    public boolean isSameSubRoot(TreeNode root, TreeNode subRoot) {
+        if (root == null || subRoot == null) {
+            return root == null && subRoot == null;
+        } else if (root.val == subRoot.val) {
+            return isSameSubRoot(root.left, subRoot.left) && isSameSubRoot(root.right, subRoot.right);
+        } else {
             return false;
         }
     }
