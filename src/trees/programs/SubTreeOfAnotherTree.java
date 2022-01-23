be2c9a09792304439a25d9b5b8d9fc1418e3f7ca
user <krishnaveer.chahar_cs19@gla.ac.in>
2022-01-23 18:07:13 +0530

    Subtree of another Tree

---
+++ src/trees/programs/SubTreeOfAnotherTree.java
@@ -0,0 +1,25 @@
+package trees.programs;
+
+import trees.TreeNode.TreeNode;
+
+public class SubTreeOfAnotherTree {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root==null){
+            return false;
+        }else if(isSameSubRoot(root,subRoot)){
+            return true;
+        }else{
+            return isSubtree(root.left,subRoot) || isSubtree(root.right, subRoot);
+        }
+    }
+
+    public boolean isSameSubRoot(TreeNode root, TreeNode subRoot){
+        if(root==null || subRoot==null){
+            return root==null && subRoot==null;
+        }else if(root.val==subRoot.val){
+            return isSameSubRoot(root.left,subRoot.left) && isSameSubRoot(root.right, subRoot.right);
+        }else{
+            return false;
+        }
+    }
+}
