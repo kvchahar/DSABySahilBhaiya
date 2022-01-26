fd3c6c673130549e13003a15eb4327eca710ccb7
user <krishnaveer.chahar_cs19@gla.ac.in>
2022-01-27 01:41:28 +0530

    Search into BST

---
+++ src/trees/programs/binarysearchtree/SearchInBST.java
@@ -0,0 +1,23 @@
+package trees.programs.binarysearchtree;
+
+import trees.TreeNode.TreeNode;
+
+public class SearchInBST {
+    public TreeNode searchBST(TreeNode root, int val) {
+
+        if(root==null){
+            return null;
+        }
+
+        if(root.val==val){
+            return root;
+        }
+
+        else if(root.val>val){
+            return searchBST(root.left, val);
+        }else{
+            return searchBST(root.right,val);
+        }
+
+    }
+}
