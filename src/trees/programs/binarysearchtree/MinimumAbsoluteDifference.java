13cdfa6c2f768f9108cee43f2fad9f60ee6efe42
user <krishnaveer.chahar_cs19@gla.ac.in>
2022-01-31 05:08:04 +0530

    Minimum Absolute Difference

--- src/trees/programs/binarysearchtree/MinimumAbsoluteDifference.java
+++ src/trees/programs/binarysearchtree/MinimumAbsoluteDifference.java
@@ -6,29 +6,58 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MinimumAbsoluteDifference {
-    public int getMinimumDifference(TreeNode root) {
+//    public int getMinimumDifference(TreeNode root) {
+//
+//        List<Integer> answer = new ArrayList<>();
+//        int minValue = Integer.MAX_VALUE;
+//        inorderTraversal(root,answer);
+//
+//        for(int i = 1; i<answer.size(); i++){
+//            if(answer.get(i)-answer.get(i-1)<minValue){
+//                minValue = answer.get(i) - answer.get(i-1);
+//            }
+//        }
+//        return minValue;
+//    }
+//
+//    private void inorderTraversal(TreeNode root,List<Integer> answer){
+//
+//        if(root==null){
+//            return;
+//        }
+//
+//        inorderTraversal(root.left,answer);
+//        answer.add(root.val);
+//        inorderTraversal(root.right,answer);
+//
+//        return;
+//    }
 
-        List<Integer> answer = new ArrayList<>();
-        int minValue = Integer.MAX_VALUE;
-        inorderTraversal(root,answer);
 
-        for(int i = 1; i<answer.size(); i++){
-            if(answer.get(i)-answer.get(i-1)<minValue){
-                minValue = answer.get(i) - answer.get(i-1);
-            }
-        }
-        return minValue;
+
+
+    // without extra space
+    public int getMinimumDifference(TreeNode root){
+        TreeNode[] prev = new TreeNode[1];
+        int[] min  = {Integer.MAX_VALUE};
+        inorder(root,prev,min);
+
+        return min[0];
     }
 
-    private void inorderTraversal(TreeNode root,List<Integer> answer){
+    private void inorder(TreeNode root, TreeNode[] prev, int[] min){
 
         if(root==null){
             return;
         }
 
-        inorderTraversal(root.left,answer);
-        answer.add(root.val);
-        inorderTraversal(root.right,answer);
+        inorder(root.left,prev,min);
+
+        if(prev[0]!=null){
+            min[0] = Math.min(min[0],Math.abs(root.val-prev[0].val));
+        }
+        prev[0] = root;
+        inorder(root.right,prev,min);
 
         return;
     }
