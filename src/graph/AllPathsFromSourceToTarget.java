07d0677db26486eebd58c8e6485f6bd2cf3f5368
user <krishnaveer.chahar_cs19@gla.ac.in>
2022-02-02 23:40:33 +0530

    All Paths from Source to Target

---
+++ src/graph/AllPathsFromSourceToTarget.java
@@ -0,0 +1,43 @@
+package graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AllPathsFromSourceToTarget {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<List<Integer>> answer = new ArrayList<>();
+        int n = graph.length;
+
+        boolean[] visited = new boolean[n];
+
+        sourceToTarget(graph, 0, visited, new ArrayList<>(), n, answer);
+
+        return answer;
+    }
+
+    private void sourceToTarget(int[][] graph, int currentNode, boolean[] visited, List<Integer> currentPath, int n, List<List<Integer>> answer) {
+
+        if (currentNode == n - 1) {
+            currentPath.add(currentNode);
+            answer.add(new ArrayList<>(currentPath));
+            currentPath.remove(currentPath.size() - 1);
+            return;
+        }
+
+        if (visited[currentNode]) {
+            return;
+        }
+
+
+        visited[currentNode] = true;
+        currentPath.add(currentNode);
+
+        for (int neighbours : graph[currentNode]) {
+            sourceToTarget(graph, neighbours, visited, currentPath, n, answer);
+
+        }
+        visited[currentNode] = false;
+        currentPath.remove(currentPath.size() - 1);
+        return;
+    }
+}
