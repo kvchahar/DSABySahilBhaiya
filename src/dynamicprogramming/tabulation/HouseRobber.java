25dc1c50b9a113c16b69c44a4cc75a576e3eaf49
user <krishnaveer.chahar_cs19@gla.ac.in>
2022-03-01 15:29:33 +0530

    House Robber

--- src/dynamicprogramming/tabulation/HouseRobber.java
+++ src/dynamicprogramming/tabulation/HouseRobber.java
@@ -3,34 +3,58 @@ package dynamicprogramming.tabulation;
 import java.util.*;
 
 public class HouseRobber {
-    static int solveUtil(int n, int[] arr, int[] dp) {
+//    static int solveUtil(int n, int[] arr, int[] dp) {
+//
+//        dp[0] = arr[0];
+//
+//        for (int i = 1; i < n; i++) {
+//            int pick = arr[i];
+//            if (i > 1)
+//                pick += dp[i - 2];
+//            int nonPick = 0 + dp[i - 1];
+//
+//            dp[i] = Math.max(pick, nonPick);
+//        }
+//
+//
+//        return dp[n - 1];
+//    }
 
-        dp[0] = arr[0];
 
-        for (int i = 1; i < n; i++) {
-            int pick = arr[i];
-            if (i > 1)
-                pick += dp[i - 2];
-            int nonPick = 0 + dp[i - 1];
+    // Space Optimization
+    public static int rob(int n, int[] nums) {
 
-            dp[i] = Math.max(pick, nonPick);
+        int prev = nums[0];
+        int prev2 = 0;
+
+
+        for(int i = 1; i<n; i++){
+            int pick = nums[i];
+
+            if(i>1){
+                pick += prev2;
+            }
+            int nonPick = prev;
+
+            int curr = Math.max(pick,nonPick);
+
+            prev2 = prev;
+            prev = curr;
         }
-
-
-        return dp[n - 1];
+        return prev;
     }
 
-    static int solve(int n, int[] arr) {
-        int dp[] = new int[n];
-        Arrays.fill(dp, -1);
-        return solveUtil(n, arr, dp);
-    }
+//    static int solve(int n, int[] arr) {
+//        int dp[] = new int[n];
+//        Arrays.fill(dp, -1);
+//        return solveUtil(n, arr, dp);
+//    }
 
 
     public static void main(String args[]) {
 
         int arr[] = {2, 1, 4, 9};
         int n = arr.length;
-        System.out.println(solve(n, arr));
+        System.out.println(rob(n, arr));
     }
 }
